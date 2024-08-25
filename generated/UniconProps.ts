49a8715b823970658c3e020c380703d63794b576
Tooni <nsayers@coveo.com>
2024-08-25 19:48:01 +0100

    use python

--- generated/UniconProps.ts
+++ generated/UniconProps.ts
@@ -1,16 +1,16 @@
+import type { SVGProps } from 'react';
 
-import type { SVGProps } from 'react'
 export interface UniconProps extends SVGProps<SVGSVGElement> {
-   /**
+    /**
      * The size of the icon.
      * Passed down to the SVG's height and width attributes.
      * @default 24
      */
-   size?: number | string;
-   /**
+    size?: number | string;
+
+    /**
      * The color of the icon.
      * @default 'currentColor'
      */
-   color?: string;
- }
- 
+    color?: string;
+}
